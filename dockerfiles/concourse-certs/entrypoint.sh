3400313affeadc63e12f4756a1542c124aa32fd5
Zenobius Jiricek <airtonix@gmail.com>
2016-12-31 03:07:36 +1030

    Fix: update template

--- dockerfiles/concourse-certs/entrypoint.sh
+++ dockerfiles/concourse-certs/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-mkdir -p /data/web
-mkdir -p /data/worker
+mkdir -p $CERT_ROOT/web
+mkdir -p $CERT_ROOT/worker
 
-[[ ! -f /data/web/tsa_host_key ]] && \
-	ssh-keygen -t rsa -f /data/web/tsa_host_key -N ''
-	cp /data/web/tsa_host_key.pub /data/worker/
+[[ ! -f $CERT_ROOT/web/tsa_host_key ]] && \
+	ssh-keygen -t rsa -f $CERT_ROOT/web/tsa_host_key -N ''
+	cp $CERT_ROOT/web/tsa_host_key.pub $CERT_ROOT/worker/
 
-[[ ! -f /data/web/session_signing_key ]] && \
-	ssh-keygen -t rsa -f /data/web/session_signing_key -N ''
+[[ ! -f $CERT_ROOT/web/session_signing_key ]] && \
+	ssh-keygen -t rsa -f $CERT_ROOT/web/session_signing_key -N ''
 
-[[ ! -f /data/worker/worker_key ]] && \
-	ssh-keygen -t rsa -f /data/worker/worker_key -N ''
-	cp /data/worker/worker_key.pub
-		/data/web/authorized_worker_keys
+[[ ! -f $CERT_ROOT/worker/worker_key ]] && \
+	ssh-keygen -t rsa -f $CERT_ROOT/worker/worker_key -N ''
+	cp $CERT_ROOT/worker/worker_key.pub
+		$CERT_ROOT/web/tsa-authorized-keys
 
 exec "${@}"
