3cebb89a788077992ade7d8f9a6f35a1ded732e0
Zenobius Jiricek <airtonix@gmail.com>
2017-01-01 02:53:50 +1030

    Fix: since it's testing only, bundle them all in same directory

--- dockerfiles/concourse-certs/entrypoint.sh
+++ dockerfiles/concourse-certs/entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-mkdir -p $CERT_ROOT/web
-mkdir -p $CERT_ROOT/worker
+mkdir -p $CERT_ROOT
 
-[[ ! -f $CERT_ROOT/web/tsa_host_key ]] && \
-	ssh-keygen -t rsa -f $CERT_ROOT/web/tsa_host_key -N ''
-	cp $CERT_ROOT/web/tsa_host_key.pub $CERT_ROOT/worker/
+[[ ! -f $CERT_ROOT/tsa_host_key ]] && \
+	ssh-keygen -t rsa -f $CERT_ROOT/tsa_host_key -N ''
 
-[[ ! -f $CERT_ROOT/web/session_signing_key ]] && \
-	ssh-keygen -t rsa -f $CERT_ROOT/web/session_signing_key -N ''
+[[ ! -f $CERT_ROOT/session_signing_key ]] && \
+	ssh-keygen -t rsa -f $CERT_ROOT/session_signing_key -N ''
 
-[[ ! -f $CERT_ROOT/worker/worker_key ]] && \
-	ssh-keygen -t rsa -f $CERT_ROOT/worker/worker_key -N ''
-	cp $CERT_ROOT/worker/worker_key.pub $CERT_ROOT/web/tsa-authorized-keys
+[[ ! -f $CERT_ROOT/worker_key ]] && \
+	ssh-keygen -t rsa -f $CERT_ROOT/worker_key -N ''
+	cat $CERT_ROOT/worker_key.pub > $CERT_ROOT/authorized_worker_keys
 
 exec "${@}"
